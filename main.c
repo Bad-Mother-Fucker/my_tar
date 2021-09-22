12a8e838d037f93a2f4560460281f78f86e3f586
Bad-Mother-Fucker <mic.desena@studenti.unina.it>
2021-09-22 19:36:59 +0200

    Add files via upload

--- main.c
+++ main.c
@@ -2,6 +2,12 @@
 #include <string.h>
 #include <stdlib.h>
 #include <sys/file.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+#ifndef BUF_SIZE
+#define BUF_SIZE 1024
+#endif
 
 typedef struct names_s {
     char** array;
@@ -11,9 +17,9 @@ typedef struct names_s {
 typedef struct metadata_s {
     size_t  size;
     mode_t  mode;
-    int owner;
-    int group;
-    time_t modification_time;
+    unsigned uid;
+    unsigned gid;
+    struct timespec modification_time;
     unsigned nameLen;
     char* name;
 }metadata;
@@ -23,13 +29,43 @@ int createArchive(char*);
 int argContains(char*, int, char*[]);
 names getFilenames(int argc, char* argv[] );
 int archive(names,int);
-int closeArchive(char*);
+int closeArchive(int);
 metadata getInfo(char*);
-int writeData(metadata, char*);
+int writeData(metadata, int);
+void copyFileContent(char* , int );
 
 
 int main(int argc, char* argv []) {
 
+    analizeArg(argc, argv);
+    return 0;
+}
+
+
+int analizeArg( int argc, char* argv []) {
+
+    if (argc < 3) {
+        argc = 4;
+        argv[1] = "-c";
+        argv[2] = "../hi.txt";
+        argv[3] = "../chichi.txt";
+    };
+
+    if (argContains("-c", argc, argv) != -1) {
+        int index = argContains("-f", argc, argv);
+        char* archiveName = index == -1 ? "../Archive.tar": argv[index+1];
+        int archiveDesc = createArchive(archiveName);
+
+
+        names filenames = getFilenames(argc,argv);
+        archive(filenames, archiveDesc);
+        closeArchive(archiveDesc);
+    }
+    return 0;
+}
+
+int closeArchive(int ad){
+    close(ad);
     return 0;
 }
 
@@ -46,8 +82,36 @@ int createArchive(char* archiveName){
 int archive(names filenames, int ad) {
     for(int i = 0; i< filenames.count; i++) {
         metadata met = getInfo(filenames.array[i]);
-        writeData(met, filenames.array[i]);
+        writeData(met, ad);
     }
+    return 0;
+}
+
+int writeData(metadata m, int ad) {
+    write(ad, "name ", sizeof(char)*5);
+    write(ad, "mode ", sizeof(char)*5);
+    write(ad, "mod time ", sizeof(char )*9);
+    //write(ad, &m.name, sizeof(char)*strlen(m.name));
+    copyFileContent(m.name, ad);
+    return 0;
+}
+
+
+
+metadata getInfo(char * filename) {
+    struct stat stat_s;
+    stat(filename, &stat_s);
+    metadata md;
+    md.gid = stat_s.st_gid;
+    md.mode = stat_s.st_mode;
+    md.modification_time = stat_s.st_mtimespec;
+    md.uid = stat_s.st_uid;
+    md.size = stat_s.st_size;
+    md.name = filename;
+    md.nameLen = strlen(filename);
+
+    return md;
+
 }
 
 int argContains(char * arg,int argc, char*argv[]) {
@@ -57,24 +121,9 @@ int argContains(char * arg,int argc, char*argv[]) {
     return -1;
 }
 
-int analizeArg( int argc, char* argv []) {
-
-   if (argc < 3) return -1;
-
-   if (strcmp(argv[1],"-c") == 0) {
-       int index = argContains("-f", argc, argv);
-       char* archiveName = index == -1 ? "Archive.tar": argv[index+1];
-       int archiveDesc = createArchive(archiveName);
-       names filenames = getFilenames(argc,argv);
-       archive(filenames, archiveDesc);
-       closeArchive(archiveName);
-   }
 
 
-
-}
-
-int isAFile(char* str) {
+int isAFilename(char* str) {
     if(strcmp(str, "-c")!=0 && strcmp(str, "-f")!=0 && strcmp(str, "-r")!=0 && strcmp(str, "-u")!=0){
         unsigned long len = strlen(str);
         const char* ext = &str[len -3];
@@ -90,13 +139,36 @@ int isAFile(char* str) {
     namesArray.count = 0;
     namesArray.array = malloc(sizeof(char *) * argc-3);
     for( int i = 1; i< argc; i++) {
-        if(isAFile(argv[i])){
-            namesArray.array[i] = (char *) malloc(sizeof(char) * (strlen(argv[i]) + 1));
-            strcpy(namesArray.array[i], argv[i]);
+        if(isAFilename(argv[i])){
+            namesArray.array[namesArray.count] = (char *) malloc(sizeof(char) * (strlen(argv[i]) + 1));
+            strcpy(namesArray.array[namesArray.count], argv[i]);
             namesArray.count ++;
         }
     }
 
     return namesArray;
+
+}
+
+
+void copyFileContent(char* filename, int ad ) {
+
+    ssize_t numRead;
+    char buf[BUF_SIZE];
+
+    /* transfer data until we encounter end of input or an error */
+    int fd = open(filename, O_RDONLY);
+    if (fd == -1) perror("Errore apertura");
+    while ((numRead = read(fd, buf, BUF_SIZE)) > 0)
+    {
+        if (write(ad, buf, numRead) != numRead)
+            perror("couldn't write whole buffer");
+    }
+
+    if (numRead == -1)
+        perror("read");
+
+    if (close(fd) == -1)
+        perror("close input");
 
 }
